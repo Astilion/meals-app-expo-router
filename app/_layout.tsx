759f7600a34a8e70e3111622cbfaad7cead18632
Mateusz Kamiński <mkaminski.contact@gmail.com>
2024-12-10 11:31:37 +0100

    recreated categories screen

---
+++ app/_layout.tsx
@@ -0,0 +1,37 @@
+import { Drawer } from "expo-router/drawer";
+import { Ionicons } from '@expo/vector-icons';
+
+
+export default function Layout() {
+  return (
+    <Drawer
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        drawerStyle: { backgroundColor: "#351401" },
+        drawerInactiveTintColor: "white",
+        drawerActiveTintColor: "#351401",
+        drawerActiveBackgroundColor: "#e4baa1",
+      }}
+    >
+      <Drawer.Screen
+        name="index"
+        options={{
+          title: "All Categories",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="favourites"
+        options={{
+          title: "Favourites",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,46 @@
+import { FlatList, StyleSheet, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import { CATEGORIES } from '../data/dummy-data';
+import Category from '../models/category';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+function CategoriesScreen() {
+  const router = useRouter();
+
+  const renderCategoryItem = (itemData: { item: Category }) => {
+    const pressHandler = () => {
+      router.push({
+        pathname: '/meals',
+        params: { categoryId: itemData.item.id },
+      });
+    };
+
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.screen}>
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: '#3f2f25',
+  },
+});
+
+export default CategoriesScreen;
